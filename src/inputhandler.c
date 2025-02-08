644198a5ac36956a5256af44915e1d4ba688be6e
DannySliekers <59461479+DannySliekers@users.noreply.github.com>
2025-02-08 18:22:52 +0100

    add text inserting between chars

--- src/inputhandler.c
+++ src/inputhandler.c
@@ -1,6 +1,7 @@
 #include "inputhandler.h"
 #include "terminalutils.h"
 #include "texteditor.h"
+#include <string.h>
 
 #define BACKSPACE 127
 #define LEFT_ARROW 68
@@ -18,13 +19,21 @@ void handle_input(char c, TextEditor* text_editor) {
             text_editor->cursor_pos--;
         }
     }
+    // Left arrow key
     else if (c == 27 && getch() == CSI && getch() == D) {
         if (text_editor->cursor_pos > 0) {
             text_editor->cursor_pos--;
         }
     }
     else {
+        char temp_lines[256];
+        memcpy(temp_lines, text_editor->lines[text_editor->line_number - 1], sizeof(temp_lines));
         text_editor->lines[text_editor->line_number - 1][text_editor->cursor_pos] = c;
+        
+        for (int i = text_editor->cursor_pos; i < 256; i++) {
+            text_editor->lines[text_editor->line_number - 1][i + 1] = temp_lines[i];
+        }
+
         text_editor->cursor_pos++;
     }
 
