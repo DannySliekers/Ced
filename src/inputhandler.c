c4624284c8fed90683fdd44b7cc6d70a05ff63f8
DannySliekers <59461479+DannySliekers@users.noreply.github.com>
2025-02-08 20:25:23 +0100

    handle rest of arrowkeys

--- src/inputhandler.c
+++ src/inputhandler.c
@@ -9,6 +9,9 @@
 #define ESCAPE 27
 // Control Sequence Initiator
 #define CSI 91
+#define A 65
+#define B 66
+#define C 67
 #define D 68
 
 void handle_input(char c, TextEditor* text_editor) {
@@ -19,13 +22,34 @@ void handle_input(char c, TextEditor* text_editor) {
             text_editor->cursor_pos--;
         }
     }
-    // Left arrow key
-    else if (c == 27 && getch() == CSI && getch() == D) {
-        if (text_editor->cursor_pos > 0) {
-            text_editor->cursor_pos--;
+    else if (c == 27 ) {
+        char potential_csi = getch();
+
+        if (potential_csi == CSI) {
+            char potential_cs = getch();
+
+            //Up arrow key
+            if (potential_cs == A) {
+                if (text_editor->line_number > 0) {
+                    text_editor->line_number--;
+                }
+            }
+            //Down arrow key
+            else if (potential_cs == B) {
+                text_editor->line_number++;
+            }
+            // Right arrow key
+            else if (potential_cs == C) {
+                text_editor->cursor_pos++;
+            }
+            //Left arrow key
+            else if (potential_cs == D) {
+                if (text_editor->cursor_pos > 0) {
+                    text_editor->cursor_pos--;
+                }
+            }
         }
-    }
-    else {
+    } else {
         char temp_lines[256];
         memcpy(temp_lines, text_editor->lines[text_editor->line_number - 1], sizeof(temp_lines));
         text_editor->lines[text_editor->line_number - 1][text_editor->cursor_pos] = c;
